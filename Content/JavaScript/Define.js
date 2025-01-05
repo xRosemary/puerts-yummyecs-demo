bb35d3730cebc9d515bd87929ef46a98207c4c66
xRosemary <2862656629@qq.com>
2025-01-05 15:45:31 +0800

    chore: 删掉临时文件

--- Content/JavaScript/Define.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DEFAULT_MAP = exports.DEFAULT_INIT_SYSTEM = void 0;
-const yummyecs_1 = require("yummyecs");
-exports.DEFAULT_INIT_SYSTEM = [yummyecs_1.F.GmSystem, yummyecs_1.F.SheetLoadSystem, yummyecs_1.F.UISystem];
-exports.DEFAULT_MAP = 'ThirdPersonMap';
-//# sourceMappingURL=Define.js.map
